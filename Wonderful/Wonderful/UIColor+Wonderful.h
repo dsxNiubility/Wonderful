d8a3e682c51db2f86c1908ddda1607684444f3d6
DongShangXian <dantesx2012@gmail.com>
2015-10-26 22:52:41 +0800

    增加黄白色系

--- Wonderful/Wonderful/UIColor+Wonderful.h
+++ Wonderful/Wonderful/UIColor+Wonderful.h
@@ -10,6 +10,26 @@
 
 @interface UIColor (Wonderful)
 
+/** 金色*/
 + (UIColor *)goldColor;
 
+/** 金麒麟色*/
++ (UIColor *)goldenrod;
+
+/** 浅金麒麟*/
++ (UIColor *)lightGoldenrod;
+/** 灰秋麒麟*/
++ (UIColor *)paleGodenrod;
+/** 卡其色*/
++ (UIColor *)khakiColor;
+/** 暗卡其色*/
++ (UIColor *)darkKhaki;
+/** 米色*/
++ (UIColor *)beigeColor;
+/** 橄榄色*/
++ (UIColor *)oliveColor;
+/** 柠檬薄纱*/
++ (UIColor *)LemonChiffon;
+/** 玉米色*/
++ (UIColor *)cornColor;
 @end
--- Wonderful/Wonderful/UIColor+Wonderful.m
+++ Wonderful/Wonderful/UIColor+Wonderful.m
@@ -11,9 +11,128 @@
 
 @implementation UIColor (Wonderful)
 
+
+#pragma mark - **************** 黄色系
+/** 金色*/
 + (UIColor *)goldColor
 {
     return SXRGB16Color(0xFFD700);
 }
+/** 金麒麟色*/
++ (UIColor *)goldenrod
+{
+    return SXRGB16Color(0xDAA520);
+}
+/** 浅金麒麟*/
++ (UIColor *)lightGoldenrod
+{
+    return SXRGB16Color(0xFAFAD2);
+}
+/** 灰秋麒麟*/
++ (UIColor *)paleGodenrod
+{
+    return SXRGB16Color(0xEEE8AA);
+}
+/** 卡其色*/
++ (UIColor *)khakiColor
+{
+    return SXRGB16Color(0xF0E68C);
+}
+/** 暗卡其色*/
++ (UIColor *)darkKhaki
+{
+    return SXRGB16Color(0xBDB76B);
+}
+/** 米色*/
++ (UIColor *)beigeColor
+{
+    return SXRGB16Color(0x6B8E23);
+}
+/** 橄榄色*/
++ (UIColor *)oliveColor
+{
+    return SXRGB16Color(0x808000);
+}
+/** 柠檬薄纱*/
++ (UIColor *)LemonChiffon
+{
+    return SXRGB16Color(0xFFFACD);
+}
+/** 玉米色*/
++ (UIColor *)cornColor
+{
+    return SXRGB16Color(0xFFF8DC);
+}
+
+#pragma mark - **************** 白色系
+/** 海贝壳*/
++ (UIColor *)seaShell
+{
+    return SXRGB16Color(0xFFF5EE);
+}
+/** 雪*/
++ (UIColor *)snowColor
+{
+    return SXRGB16Color(0xFFFAFA);
+}
+/** 亚麻色*/
++ (UIColor *)linenColor
+{
+    return SXRGB16Color(0xFAF0E6);
+}
+/** 花之白*/
++ (UIColor *)floralWhite
+{
+    return SXRGB16Color(0xFFFAF0);
+}
+/** 老饰带*/
++ (UIColor *)oldLace
+{
+    return SXRGB16Color(0xFDF5E6);
+}
+/** 象牙白*/
++ (UIColor *)ivoryColor
+{
+    return SXRGB16Color(0xFFFFF0);
+}
+/** 蜂蜜露*/
++ (UIColor *)honeydew
+{
+    return SXRGB16Color(0xF0FFF0);
+}
+/** 薄荷奶油*/
++ (UIColor *)mintCream
+{
+    return SXRGB16Color(0x00FF7F);
+}
+/** 蔚蓝色*/
++ (UIColor *)azureColor
+{
+    return SXRGB16Color(0xF0FFFF);
+}
+/** 爱丽丝蓝*/
++ (UIColor *)aliceBlue
+{
+    return SXRGB16Color(0xF0F8FF);
+}
+/** 幽灵白*/
++ (UIColor *)ghostWhite
+{
+    return SXRGB16Color(0xF8F8FF);
+}
+/** 淡紫红*/
++ (UIColor *)lavenderBlush
+{
+    return SXRGB16Color(0xFFF0F5);
+}
+/** 薰衣草*/
++ (UIColor *)lavender
+{
+    return SXRGB16Color(0xE6E6FA);
+}
+
+
+
+
 
 @end
