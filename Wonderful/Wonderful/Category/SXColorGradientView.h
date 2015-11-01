5921993bb05ff499cc90a50ccae88391b2735aaf
DongShangXian <dantesx2012@gmail.com>
2015-11-01 20:21:33 +0800

    建了一个文件用来处理渐变色

---
+++ Wonderful/Wonderful/Category/SXColorGradientView.h
@@ -0,0 +1,13 @@
+//
+//  SXColorGradientView.h
+//  Wonderful
+//
+//  Created by dongshangxian on 15/11/1.
+//  Copyright © 2015年 Sankuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXColorGradientView : UIView
+
+@end
---
+++ Wonderful/Wonderful/Category/SXColorGradientView.m
@@ -0,0 +1,21 @@
+//
+//  SXColorGradientView.m
+//  Wonderful
+//
+//  Created by dongshangxian on 15/11/1.
+//  Copyright © 2015年 Sankuai. All rights reserved.
+//
+
+#import "SXColorGradientView.h"
+
+@implementation SXColorGradientView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
