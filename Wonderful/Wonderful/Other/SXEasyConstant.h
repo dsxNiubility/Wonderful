cdf1fc3257451dc0a0e21369b75d51bfb872878b
DongShangXian <dantesx2012@gmail.com>
2015-11-05 17:40:24 +0800

    引入四个工具类 开始更效率的编程

---
+++ Wonderful/Wonderful/Other/SXEasyConstant.h
@@ -0,0 +1,32 @@
+//
+//  SXEasyConstant.h
+//  SXEasyMacroDemo
+//
+//  Created by dongshangxian on 15/10/17.
+//  Copyright © 2015年 Sankuai. All rights reserved.
+//
+
+#ifndef SXEasyConstant_h
+#define SXEasyConstant_h
+
+#define NavigationBarHeight 44
+#define StatusBarHeight 20
+#define TopBarHeight 64
+#define ToolBarHeight 44
+#define TabBarHeight 49
+
+#define iPhone4_W 320
+#define iPhone4_H 480
+#define iPhone5_W 320
+#define iPhone5_H 568
+#define iPhone6_W 375
+#define iPhone6_H 667
+#define iPhone6P_W 414
+#define iPhone6P_H 736
+
+#define ScreenMargin 16
+#define Bit 1024
+#define DefaultFontSize 17
+
+
+#endif /* SXEasyConstant_h */
