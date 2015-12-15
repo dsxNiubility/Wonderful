d65c7c92b5420a0ddf180f47c923d755dd9e3b10
DongShangXian <dantesx2012@gmail.com>
2015-12-15 12:11:00 +0800

    添加方法注释

--- Wonderful/Wonderful/Category/SXColorGradientView.h
+++ Wonderful/Wonderful/Category/SXColorGradientView.h
@@ -17,8 +17,23 @@ typedef NS_ENUM(NSInteger, SXColorGradientDirection) {
 
 @interface SXColorGradientView : UIView
 
+/**
+ *  use one color to init the gradientView,default is this color to clear.
+ *
+ *  @param visible   can you see other view what below the gradientView
+ *  @param direction  the direction we want gradient to clear
+ *
+ *  @return
+ */
 + (instancetype)createWithColor:(UIColor *)color frame:(CGRect)frame visible:(BOOL)visible direction:(SXColorGradientDirection)direction;
 
+/**
+ *  use two color to init the gradientView, from fromColor gradient to toColor
+ *
+ *  @param direction the direction we want gradient to toColor
+ *
+ *  @return 
+ */
 + (instancetype)createWithFromColor:(UIColor *)fromColor toColor:(UIColor *)toColor frame:(CGRect)frame direction:(SXColorGradientDirection)direction;
 
 @end
