5dda86cc3b81bcad1930aa79e106bd445462b8fd
DongShangXian <dantesx2012@gmail.com>
2015-12-15 12:04:38 +0800

    给跑马灯的方法增加注释

--- Wonderful/Wonderful/Category/SXMarquee.h
+++ Wonderful/Wonderful/Category/SXMarquee.h
@@ -17,14 +17,52 @@ typedef NS_ENUM(NSInteger, SXMarqueeSpeedLevel) {
 
 @interface SXMarquee : UIView
 
-- (instancetype)initWithFrame:(CGRect)frame speed:(SXMarqueeSpeedLevel)speed Msg:(NSString *)msg bgColor:(UIColor *)bgColor txtColor:(UIColor *)txtColor;
-
+/**
+ *  style is default, backgroundColor is white,textColor is black;
+ *
+ *  @param speed you can set 2,4,6,8.  smaller is faster
+ *
+ *  @return
+ */
 - (instancetype)initWithFrame:(CGRect)frame speed:(SXMarqueeSpeedLevel)speed Msg:(NSString *)msg ;
 
+/**
+ *  style is diy, backgroundColor and textColor can config
+ *
+ *  @param speed  you can set 2,4,6,8.  smaller is faster
+ *  @param bgColor  backgroundColor
+ *  @param txtColor textColor
+ *
+ *  @return
+ */
+- (instancetype)initWithFrame:(CGRect)frame speed:(SXMarqueeSpeedLevel)speed Msg:(NSString *)msg bgColor:(UIColor *)bgColor txtColor:(UIColor *)txtColor;
+
+/**
+ *  you can change the tapAction show or jump, without this method default is tap to stop
+ *
+ *  @param action tapAction block code
+ */
 - (void)changeTapMarqueeAction:(void(^)())action;
+
+/**
+ *  you can change marqueeLabel 's font before start
+ *
+ */
 - (void)changeMarqueeLabelFont:(UIFont *)font;
 
+/**
+ *  when you set everything what you want,you can use this method to begin animate
+ */
 - (void)start;
+
+/**
+ *  pause
+ */
 - (void)stop;
+
+/**
+ *  will start with the point we stoped.
+ */
 - (void)restart;
+
 @end
