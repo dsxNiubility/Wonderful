d4a135bddf293223ad146dbda5607206a16d75dc
DongShangXian <dantesx2012@gmail.com>
2015-12-19 15:50:32 +0800

    实现了核心功能，可以把添加的文字自动转化为部分彩色文字

--- Wonderful/Wonderful/Category/SXColorLabel.m
+++ Wonderful/Wonderful/Category/SXColorLabel.m
@@ -13,7 +13,27 @@
 - (void)setText:(NSString *)text
 {
     [super setText:text];
-
+    NSMutableString *mstr = [NSMutableString string];
+    [mstr appendString:text];
+    
+    NSRange range1 = [text rangeOfString:@"<"];
+    NSRange range2 = [text rangeOfString:@">"];
+    NSUInteger location =0;
+    NSUInteger length = 0;
+    if (range1.location != NSNotFound) {
+        location = range1.location;
+        length = range2.location - range1.location-1;
+        [mstr replaceOccurrencesOfString:@"<" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, mstr.length)];
+        [mstr replaceOccurrencesOfString:@">" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, mstr.length)];
+    }
+    NSMutableAttributedString *AttributedStr = [[NSMutableAttributedString alloc]initWithString:mstr];
+    [AttributedStr addAttribute:NSForegroundColorAttributeName
+                          value:[UIColor orangeColor]
+                          range:NSMakeRange(location, length)];
+    [AttributedStr addAttribute:NSFontAttributeName
+                          value:[UIFont boldSystemFontOfSize:15]
+                          range:NSMakeRange(location, length)];
+    [self setAttributedText:AttributedStr];
 }
 
 @end
