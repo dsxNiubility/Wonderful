aa1c7e43bb6ebdbd359e6818edcf7341d18967a1
DongShangXian <dantesx2012@gmail.com>
2015-12-19 15:30:58 +0800

    新建了colorLabel 准备实现新功能

---
+++ Wonderful/Wonderful/Category/SXColorLabel.h
@@ -0,0 +1,13 @@
+//
+//  SXColorLabel.h
+//  Wonderful
+//
+//  Created by dongshangxian on 15/12/19.
+//  Copyright © 2015年 Sankuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXColorLabel : UILabel
+
+@end
---
+++ Wonderful/Wonderful/Category/SXColorLabel.m
@@ -0,0 +1,21 @@
+//
+//  SXColorLabel.m
+//  Wonderful
+//
+//  Created by dongshangxian on 15/12/19.
+//  Copyright © 2015年 Sankuai. All rights reserved.
+//
+
+#import "SXColorLabel.h"
+
+@implementation SXColorLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
