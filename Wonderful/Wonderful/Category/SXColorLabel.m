783db3aec66cea44d5cd3c3e054b086999fe9259
DongShangXian <dantesx2012@gmail.com>
2015-12-19 16:07:42 +0800

    重构代码，现在可以支持一个str中出现多处高亮颜色

--- Wonderful/Wonderful/Category/SXColorLabel.m
+++ Wonderful/Wonderful/Category/SXColorLabel.m
@@ -15,26 +15,45 @@
     if (([text rangeOfString:@"<"].location != NSNotFound)||([text rangeOfString:@"["].location != NSNotFound)) {
         NSMutableString *mstr = [NSMutableString string];
         [mstr appendString:text];
+        NSMutableAttributedString *AttributedStr;
+        
         NSRange range1;
         NSRange range2;
         NSUInteger location =0;
         NSUInteger length = 0;
-        if (range1.location != NSNotFound) {
+        
+        NSMutableArray *rangeArray = [NSMutableArray array];
+        
+        while (range1.location != NSNotFound) {
             range1 = [mstr rangeOfString:@"<"];
             range2 = [mstr rangeOfString:@">"];
             
-            location = range1.location;
-            length = range2.location - range1.location-1;
-            [mstr replaceOccurrencesOfString:@"<" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, mstr.length)];
-            [mstr replaceOccurrencesOfString:@">" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, mstr.length)];
+            if (range1.location != NSNotFound) {
+                location = range1.location;
+                length = range2.location - range1.location-1;
+                
+                [mstr replaceOccurrencesOfString:@"<" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, range1.location + range1.length)];
+                [mstr replaceOccurrencesOfString:@">" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, range2.location + range2.length - 1)];
+            }
+            if (length > 5000) {
+                break;
+            }
+            NSDictionary *dict = @{@"location":@(location),@"length":@(length)};
+            [rangeArray addObject:dict];
         }
-        NSMutableAttributedString *AttributedStr = [[NSMutableAttributedString alloc]initWithString:mstr];
-        [AttributedStr addAttribute:NSForegroundColorAttributeName
-                              value:[UIColor orangeColor]
-                              range:NSMakeRange(location, length)];
-        [AttributedStr addAttribute:NSFontAttributeName
-                              value:[UIFont boldSystemFontOfSize:15]
-                              range:NSMakeRange(location, length)];
+        [mstr replaceOccurrencesOfString:@"<" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, mstr.length)];
+        [mstr replaceOccurrencesOfString:@">" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, mstr.length)];
+        AttributedStr = [[NSMutableAttributedString alloc]initWithString:mstr];
+        for (NSDictionary *dict in rangeArray) {
+            NSUInteger lo = [dict[@"location"] integerValue];
+            NSUInteger le = [dict[@"length"] integerValue];
+            [AttributedStr addAttribute:NSForegroundColorAttributeName
+                                  value:[UIColor redColor]
+                                  range:NSMakeRange(lo, le)];
+            [AttributedStr addAttribute:NSFontAttributeName
+                                  value:[UIFont boldSystemFontOfSize:15]
+                                  range:NSMakeRange(lo, le)];
+        }
         [self setAttributedText:AttributedStr];
     }else{
         [super setText:text];
