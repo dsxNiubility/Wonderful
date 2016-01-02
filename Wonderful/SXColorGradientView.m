12a97537eed54655c3669be60779ab8b102ece1e
DongShangXian <dantesx2012@gmail.com>
2016-01-02 18:00:35 +0800

    重构构造方法

--- Wonderful/SXColorGradientView.m
+++ Wonderful/SXColorGradientView.m
@@ -12,49 +12,15 @@
 @implementation SXColorGradientView
 
 + (instancetype)createWithColor:(UIColor *)color frame:(CGRect)frame direction:(SXGradientDirection)direction{
-    return [[self alloc]initWithFromColor:color toColor:[color down:SXColorTypeAlpha num:255] frame:frame direction:direction];
+    return [[self alloc]initWithColorArray:@[color,[color down:SXColorTypeAlpha num:255]] frame:frame direction:direction];
 }
 
 + (instancetype)createWithFromColor:(UIColor *)fromColor toColor:(UIColor *)toColor frame:(CGRect)frame direction:(SXGradientDirection)direction{
-    return [[self alloc]initWithFromColor:fromColor toColor:toColor frame:frame direction:direction];
+    return [[self alloc]initWithColorArray:@[fromColor,toColor] frame:frame direction:direction];
 }
 
 + (instancetype)createWithColorArray:(NSArray *)colorArray frame:(CGRect)frame direction:(SXGradientDirection)direction{
     return [[self alloc]initWithColorArray:colorArray frame:frame direction:direction];
-}
-
-- (instancetype)initWithFromColor:(UIColor *)fromColor toColor:(UIColor *)toColor frame:(CGRect)frame direction:(SXGradientDirection)direction{
-    if (self = [super initWithFrame:frame]) {
-        SXColorGradientView *gradientView = [[SXColorGradientView alloc]initWithFrame:frame];
-        self = gradientView;
-        CAGradientLayer *la = [[CAGradientLayer alloc]init];
-        la.frame = gradientView.bounds;
-        [gradientView.layer addSublayer:la];
-        la.colors = @[(__bridge id)fromColor.CGColor,(__bridge id)toColor.CGColor];
-        
-        switch (direction) {
-            case 1:
-                la.startPoint = CGPointMake(0.5, 1);
-                la.endPoint = CGPointMake(0.5, 0);
-                break;
-            case 2:
-                la.startPoint = CGPointMake(1, 0.5);
-                la.endPoint = CGPointMake(0, 0.5);
-                break;
-            case 3:
-                la.startPoint = CGPointMake(0.5, 0);
-                la.endPoint = CGPointMake(0.5, 1);
-                break;
-            case 4:
-                la.startPoint = CGPointMake(0, 0.5);
-                la.endPoint = CGPointMake(1, 0.5);
-                break;
-                
-            default:
-                break;
-        }
-    }
-    return self;
 }
 
 - (instancetype)initWithColorArray:(NSArray *)colorArray frame:(CGRect)frame direction:(SXGradientDirection)direction{
