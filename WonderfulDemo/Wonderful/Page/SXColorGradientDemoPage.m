b252f411edbcb8b598cbdabc135b6068f330110d
DongShangXian <dantesx2012@gmail.com>
2016-01-02 18:56:18 +0800

    更新gradientDemoPage页面

--- WonderfulDemo/Wonderful/Page/SXColorGradientDemoPage.m
+++ WonderfulDemo/Wonderful/Page/SXColorGradientDemoPage.m
@@ -20,7 +20,13 @@
 
 @property(nonatomic,weak)SXColorGradientView *grv5;
 
+@property(nonatomic,weak)SXColorGradientView *grv6;
+@property(nonatomic,weak)SXColorGradientView *grv7;
+
 @property(nonatomic,strong)SXDataSource *dataSource;
+
+@property (weak, nonatomic) IBOutlet UILabel *msgLabel;
+
 @end
 
 @implementation SXColorGradientDemoPage
@@ -30,8 +36,22 @@
     self.title = @"SXColorGradientView";
     
     self.dataSource = [SXDataSource new];
+    self.msgLabel.text = @"a.支持只传一个颜色默认往透明渐变。\nb.支持传两个颜色相互渐变。\nc.支持传一个数组，自动计算渐变。\nd.支持4种渐变方向，后续还会扩展。";
     [self randomGradient];
-    // Do any additional setup after loading the view.
+    
+    
+//    SXCLEntity *item5 = [self.dataSource randomAColorEntity];
+//    SXColorGradientView *grv51 = [SXColorGradientView createWithColor:SXRGB16Color(item5.colorValue) frame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height - 255, 375, 255) direction:SXGradientToTop];
+//    NSArray *colorArray = @[[UIColor redColor],[UIColor orangeColor],[UIColor yellowColor],[UIColor greenColor],[UIColor blueColor],[UIColor purpleColor]];
+    NSArray *colorArray = @[Wonderful_BlueColor4,Wonderful_PurpleColor5,Wonderful_PinkColor5,Wonderful_RedColor5,Wonderful_YelloeColor5,Wonderful_GreenColor3,Wonderful_CyanColor4];
+    CGRect rect = CGRectMake(0, [UIScreen mainScreen].bounds.size.height - 255, 375, 255);
+    SXColorGradientView *grv5 = [SXColorGradientView createWithColorArray:colorArray frame:rect direction:SXGradientToRight];
+    SXColorGradientView *grv5_5 = [SXColorGradientView createWithColor:[UIColor whiteColor] frame:rect direction:SXGradientToBottom];
+    
+    [self.view addSubview:grv5];
+    [self.view addSubview:grv5_5];
+//    self.grv5 = grv5;
+    
 }
 
 - (IBAction)randomGradient {
@@ -39,12 +59,16 @@
     [self.grv2 removeFromSuperview];
     [self.grv3 removeFromSuperview];
     [self.grv4 removeFromSuperview];
-    [self.grv5 removeFromSuperview];
+    [self.grv6 removeFromSuperview];
+    [self.grv7 removeFromSuperview];
+
     self.grv1 = nil;
     self.grv2 = nil;
     self.grv3 = nil;
     self.grv4 = nil;
-    self.grv5 = nil;
+    self.grv6 = nil;
+    self.grv7 = nil;
+
     
     SXCLEntity *item1 = [self.dataSource randomAColorEntity];
     SXColorGradientView *grv1 = [SXColorGradientView createWithColor:SXRGB16Color(item1.colorValue) frame:CGRectMake(10, 10, 80, 30) direction:SXGradientToRight];
@@ -65,10 +89,19 @@
     [self.gradientContent addSubview:grv4];
     
     
-    SXCLEntity *item5 = [self.dataSource randomAColorEntity];
-    SXColorGradientView *grv5 = [SXColorGradientView createWithColor:SXRGB16Color(item5.colorValue) frame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height - 255, 375, 255) direction:SXGradientToTop];
-    [self.view addSubview:grv5];
+    SXCLEntity *item6 = [self.dataSource randomAColorEntity];
+    NSArray *array6 = @[SXRGB16Color(item1.colorValue),SXRGB16Color(item2.colorValue),SXRGB16Color(item6.colorValue)];
+    SXColorGradientView *grv6 = [SXColorGradientView createWithColorArray:array6 frame:CGRectMake(10, 100, 335, 30) direction:SXGradientToRight];
+    [self.gradientContent addSubview:grv6];
     
+    SXCLEntity *item71 = [self.dataSource randomAColorEntity];
+    SXCLEntity *item72 = [self.dataSource randomAColorEntity];
+    SXCLEntity *item73 = [self.dataSource randomAColorEntity];
+    NSArray *array7 = @[SXRGB16Color(item31.colorValue),SXRGB16Color(item41.colorValue),SXRGB16Color(item71.colorValue),SXRGB16Color(item72.colorValue),SXRGB16Color(item73.colorValue)];
+    SXColorGradientView *grv7 = [SXColorGradientView createWithColorArray:array7 frame:CGRectMake(10, 140, 335, 30) direction:SXGradientToRight];
+    [self.gradientContent addSubview:grv7];
+    
+
     grv1.layer.cornerRadius = 2;
     grv1.layer.masksToBounds = YES;
     self.grv1 = grv1;
@@ -84,7 +117,15 @@
     grv4.layer.cornerRadius = 2;
     grv4.layer.masksToBounds = YES;
     self.grv4 = grv4;
-    self.grv5 = grv5;
+    
+    grv6.layer.cornerRadius = 2;
+    grv6.layer.masksToBounds = YES;
+    self.grv6 = grv6;
+    
+    grv7.layer.cornerRadius = 2;
+    grv7.layer.masksToBounds = YES;
+    self.grv7 = grv7;
+
 }
 
 /*
