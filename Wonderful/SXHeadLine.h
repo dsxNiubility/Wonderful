17ec50625c34e16f80f4ee31778193802e4c25a2
DongShangXian <dantesx2012@gmail.com>
2016-02-18 10:29:22 +0800

    建立headline组件用来展示往上滚动的头条栏

---
+++ Wonderful/SXHeadLine.h
@@ -0,0 +1,13 @@
+//
+//  SXHeadLine.h
+//  Wonderful
+//
+//  Created by dongshangxian on 16/2/18.
+//  Copyright © 2016年 Sankuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXHeadLine : UIView
+
+@end
---
+++ Wonderful/SXHeadLine.m
@@ -0,0 +1,21 @@
+//
+//  SXHeadLine.m
+//  Wonderful
+//
+//  Created by dongshangxian on 16/2/18.
+//  Copyright © 2016年 Sankuai. All rights reserved.
+//
+
+#import "SXHeadLine.h"
+
+@implementation SXHeadLine
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
